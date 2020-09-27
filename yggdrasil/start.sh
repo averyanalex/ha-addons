5aa0d7167ec5cbbec07e91fbd15650b5139b846b
Александр Аверьянов <averyanalex@gmail.com>
2020-09-27 12:16:20 +0300

    Update start.sh

--- yggdrasil/start.sh
+++ yggdrasil/start.sh
@@ -4,8 +4,4 @@ conf_file=/config/yggdrasil.conf
 
 [ -f "${conf_file}" ] || yggdrasil -genconf >${conf_file}
 
-modprobe tun
-mkdir /dev/net
-mknod /dev/net/tun c 10 200
-ip tuntap add mode tap tap
 yggdrasil -useconffile ${conf_file}
