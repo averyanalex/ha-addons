01254675cac3dc8e5dc81a0044ee3e3226211a80
Александр Аверьянов <averyanalex@gmail.com>
2020-09-30 10:08:12 +0300

    Build from git

--- yggdrasil/build.sh
+++ yggdrasil/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --rm --privileged -v ~/.docker:/root/.docker -v /home/alex/Документы/Программирование/ha-addons/yggdrasil:/data homeassistant/amd64-builder --all -t /data -i ha-yggdrasil-{arch} -d averyanalex --docker-hub-check --no-cache
+sudo docker run --rm --privileged -v ~/.docker:/root/.docker homeassistant/amd64-builder --all -r https://github.com/averyanalex/ha-addons -t yggdrasil -i ha-yggdrasil-{arch} -d averyanalex --docker-hub-check --no-cache
