5dc08b50a77228093f9477b613772ccd31dcbe46
Alex Averyanov <averyanalex@gmail.com>
2022-02-10 11:40:32 +0300

    Dont print config on normalization

--- yggdrasil/run.sh
+++ yggdrasil/run.sh
@@ -5,7 +5,7 @@ conf_file=/data/yggdrasil.conf
 [ -f "${conf_file}" ] || /usr/local/bin/yggdrasil -genconf > ${conf_file}
 
 echo Normalizing config...
-/usr/local/bin/yggdrasil -normaliseconf -useconffile /etc/yggdrasil.conf | tee /etc/yggdrasil.conf
+/usr/local/bin/yggdrasil -normaliseconf -useconffile /etc/yggdrasil.conf > /etc/yggdrasil.conf
 
 echo Yggdrasil IP:
 /usr/local/bin/yggdrasil -address -useconffile ${conf_file}
