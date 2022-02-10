20b474eb667b703031674d26aad014ee9872d11f
Alex Averyanov <averyanalex@gmail.com>
2022-02-10 11:29:32 +0300

    Config normalization

--- yggdrasil/run.sh
+++ yggdrasil/run.sh
@@ -4,6 +4,9 @@ conf_file=/data/yggdrasil.conf
 
 [ -f "${conf_file}" ] || /usr/local/bin/yggdrasil -genconf > ${conf_file}
 
+echo Normalizing config...
+/usr/local/bin/yggdrasil -normaliseconf -useconffile /etc/yggdrasil.conf | tee /etc/yggdrasil.conf
+
 echo Yggdrasil IP:
 /usr/local/bin/yggdrasil -address -useconffile ${conf_file}
 
